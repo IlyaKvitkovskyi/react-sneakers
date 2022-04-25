c52d604b4165f0ef7de1d47f6e5876191ec3fbf2
Illia Kvitkovskyi <kvitkovskii97@gmail.com>
2022-04-25 23:38:06 +0300

    added basket

--- src/App.js
+++ src/App.js
@@ -3,7 +3,56 @@ function App() {
     <div className="wrapper clear">
       <div className="overlay">
         <div className="drawer">
-          <h3>Корзина</h3>
+          <h2 className="mb-30 d-flex justify-between">
+            Корзина
+            <img className="removeBtn cu-p" src="/img/btn-remove.svg" alt="Remove" />
+          </h2>
+
+          <div className="items">
+            <div className="cartItem d-flex align-center mb-20">
+              <div
+                style={{ backgroundImage: 'url(/img/sneakers/sneakers-2.jpg)' }}
+                className="cartItemImg"></div>
+
+              <div className="mr-20 flex">
+                <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
+                <b>12 999 грн.</b>
+              </div>
+              <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove" />
+            </div>
+
+            <div className="cartItem d-flex align-center mb-20">
+              <div
+                style={{ backgroundImage: 'url(/img/sneakers/sneakers-3.jpg)' }}
+                className="cartItemImg"></div>
+
+              <div className="mr-20 flex">
+                <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
+                <b>12 999 грн.</b>
+              </div>
+              <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove" />
+            </div>
+          </div>
+
+          <div className="cartTotalBlock">
+            <ul>
+              <li>
+                <span>Итого:</span>
+                <div></div>
+                <b>21 498 грн.</b>
+              </li>
+              <li>
+                <span>Налог 5%:</span>
+                <div></div>
+                <b>1074 грн.</b>
+              </li>
+            </ul>
+
+            <button className="greenButton">
+              Оформить заказ
+              <img src="img/arrow.svg" alt="Arrow" />
+            </button>
+          </div>
         </div>
       </div>
 
