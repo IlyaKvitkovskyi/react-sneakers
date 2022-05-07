daa87966dbeaf8213ea85d4a6a35a9431acec3e4
Illia Kvitkovskyi <kvitkovskii97@gmail.com>
2022-05-07 20:28:13 +0300

    added changes to Card and add scss modules

--- src/App.js
+++ src/App.js
@@ -4,10 +4,25 @@ import Drawer from './components/Drawer';
 
 const arr = [
   {
-    name: 'Мужские Кроссовки Nike Blazer Mid Suede',
+    title: 'Мужские Кроссовки Nike Blazer Mid Suede',
     price: 12999,
+    imageUrl: '/img/sneakers/sneakers.jpg',
   },
-  { name: 'Мужские Кроссовки Nike Air Max 270', price: 15600 },
+  {
+    title: 'Мужские Кроссовки Nike Air Max 270',
+    price: 15600,
+    imageUrl: '/img/sneakers/sneakers-2.jpg',
+  },
+  {
+    title: 'Мужские Кроссовки Nike Blazer Mid Suede',
+    price: 8499,
+    imageUrl: '/img/sneakers/sneakers-3.jpg',
+  },
+  {
+    title: 'Кроссовки Puma X Aka Boku Future Rider',
+    price: 8999,
+    imageUrl: '/img/sneakers/sneakers-4.jpg',
+  },
 ];
 
 function App() {
@@ -26,9 +41,14 @@ function App() {
         </div>
 
         <div className="d-flex">
-         {
-           
-         }
+          {arr.map((obj) => (
+            <Card
+              title={obj.title}
+              price={obj.price}
+              imageUrl={obj.imageUrl}
+              onClick={() => console.log(obj)}
+            />
+          ))}
         </div>
       </div>
     </div>
